67d29ab427771cffddba554474003ecc182b9055
Thomas Leu <thleu@student.ethz.ch>
2021-03-05 10:14:23 +0100

    fixed bug in validate.js

--- layouter_dir/node_scripts/validate.js
+++ layouter_dir/node_scripts/validate.js
@@ -1,6 +1,6 @@
 const load = require('./nodeLoader');
 require('../dist/layoutLib');
-const layouter = new layoutLib.DagreLayouter();
-layoutLib.Bench.validate(load, layouter, ["lulesh-with-maps"]).catch((e) => {
+const layouter = new layoutLib.layouter.DagreLayouter();
+layoutLib.Bench.validate(load, layouter).catch((e) => {
     console.error(e);
 });
