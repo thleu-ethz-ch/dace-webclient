1df0370848c3c1d9d3408d13e5d3f51cf0c755dc
Thomas Leu <thleu@student.ethz.ch>
2021-05-13 19:08:23 +0200

    added npbench graphs

--- layouter_dir/src/bench/bench.ts
+++ layouter_dir/src/bench/bench.ts
@@ -10,6 +10,7 @@ export default class Bench {
     public static GRPAHS_MEDIUM = ["bert2", "encbwd", "unreadable", "VA-gpu"]
     public static GRAPHS_LARGE = ["bert", "lulesh-with-maps", "rgf_dense"];
     public static GRAPHS_ALL = ["bert", "bert2", "encbwd", "gemm_opt", "jacobi", "lulesh-with-maps", "placement", "rgf_dense", "symm", "syrk", "trisolv", "trmm", "unreadable", "VA-gpu", "wrong"];
+    public static GRAPHS_POLYBENCH = ["npbench/polybench/adi", "npbench/polybench/atax", "npbench/polybench/bicg", "npbench/polybench/cholesky", "npbench/polybench/correlation", "npbench/polybench/covariance", "npbench/polybench/deriche", "npbench/polybench/doitgen", "npbench/polybench/durbin", "npbench/polybench/fdtd_2d", "npbench/polybench/floyd_warshall", "npbench/polybench/gemm", "npbench/polybench/gemver", "npbench/polybench/gesummv", "npbench/polybench/gramschmidt", "npbench/polybench/heat_3d", "npbench/polybench/jacobi_1d", "npbench/polybench/jacobi_2d", "npbench/polybench/k2mm", "npbench/polybench/k3mm", "npbench/polybench/lu", "npbench/polybench/ludcmp", "npbench/polybench/mvt", "npbench/polybench/nussinov", "npbench/polybench/seidel_2d", "npbench/polybench/symm", "npbench/polybench/syr2k", "npbench/polybench/syrk", "npbench/polybench/trisolv", "npbench/polybench/trmm"];
 
     public static LAYOUTERS = [new DagreLayouter()];
 
